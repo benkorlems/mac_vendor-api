0d2a28c54e2c12e28d32d68174639d929d3d312d
Ebenezer Ejukorlem <talk2benzo@yahoo.com>
2018-11-06 13:14:50 +0100

    added CORS support

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
+var cors = require("cors");
 
+app.use(cors());
 // Enable parsing of Json & urlencoded params
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
